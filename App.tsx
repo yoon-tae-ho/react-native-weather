a2a4659b4ac1e51e09e789bc3ea9279960a7f642
yoontaeho <pln0302@yonsei.ac.kr>
2022-12-22 12:22:07 +0900

    Finished weather app

--- App.tsx
+++ App.tsx
@@ -1,28 +1,149 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello React Native!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const API_KEY = "095037fa1f35ee79edfa8cb86207ad4d";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "tomato",
   },
-  text: {
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
     fontSize: 40,
+    fontWeight: "700",
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temperature: {
+    fontSize: 156,
+    marginTop: 70,
+  },
+  weatherMain: {
+    fontSize: 54,
+    fontWeight: "500",
+  },
+  weatherDescription: {
+    fontSize: 20,
   },
 });
 
-// React-Native는 브라우저가 아니다.
-// 1. <div></div>는 없다. <View></View>를 사용.
-// 2. text를 적을 때는 항상 <Text></Text>를 사용해야 한다.
-// 3. 브라우저의 CSS property를 전부 다 사용할 수는 없다. (약 98%)
+interface IDay {
+  dt: number;
+  sunrise: number;
+  sunset: number;
+  temp: {
+    day: number;
+    min: number;
+    max: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  feels_like: {
+    day: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  pressure: number;
+  humidity: number;
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  speed: number;
+  deg: number;
+  gust: number;
+  clouds: number;
+  pop: number;
+  rain: number;
+}
+
+export default function App() {
+  const [region, setRegion] = useState<string | null>(null);
+  const [isOk, setIsOk] = useState<boolean>(false);
+  const [days, setDays] = useState<IDay[] | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") return setIsOk(false);
+
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      const location = await Location.reverseGeocodeAsync(
+        { latitude, longitude },
+        { useGoogleMaps: false }
+      );
+      setRegion(location[0].region);
+
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&cnt=${7}&appid=${API_KEY}&units=metric&mode=json`
+        );
+        const data = await response.json();
+        const list = data.list as IDay[];
+        setDays(list);
+        console.log(list);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
+  return (
+    // Weather App
+    <View style={styles.container}>
+      {/* City */}
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      {/* Information */}
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        {!days ? (
+          <View style={styles.day}>
+            <ActivityIndicator size="large" />
+          </View>
+        ) : (
+          days.map((day) => (
+            <View key={day.dt} style={styles.day}>
+              {/* Temperature */}
+              <Text style={styles.temperature}>{day.temp.day.toFixed(1)}</Text>
+              {/* Description */}
+              <Text style={styles.weatherMain}>{day.weather[0].main}</Text>
+              <Text style={styles.weatherDescription}>
+                {day.weather[0].description}
+              </Text>
+            </View>
+          ))
+        )}
+      </ScrollView>
+    </View>
+  );
+}
